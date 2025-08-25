e4ae9021272675a835679efc729d54b3fb343986
Pranay-ai <pranayguda39@gmail.com>
2025-08-24 17:43:03 -0700

    ""

--- next.config.mjs
+++ next.config.mjs
@@ -1,16 +1,4 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  images: {
-    remotePatterns: [
-      {
-        protocol: "https",
-        hostname:
-          "https://portfolio-images-pranay.s3.us-west-1.amazonaws.com/distributed-rate-limiter-no-text.svg?response-content-disposition=inline&X-Amz-Content-Sha256=UNSIGNED-PAYLOAD&X-Amz-Security-Token=IQoJb3JpZ2luX2VjEPj%2F%2F%2F%2F%2F%2F%2F%2F%2F%2FwEaCXVzLXdlc3QtMSJHMEUCIGSxt%2Fi9xrVW34a%2F0yBvjbQpmDmla2uL8NgBgL7VXrInAiEA2cnpENOnb%2BvVp8XJsW9lT6LXYdAYoReWSFybXjyzqQYq0AMIURABGgwwODU2Nzc5NzMyNjgiDPjNj1WK7ISQwGWEwyqtA4lzIi5J5EMBOVJkRMj5DIPeyRMSorLCB%2Bj5PFiTW2fdScxSmfEG7eeAuRX3h98tR%2FJnwMqnwS84xj5Eaio7TXVbuP33IfqwF4F7eSURjTV0Py4smDcDGyoQmwXiIh8qfAz40mwWw56%2B0sFD1WGPIWqU%2FwqaSKPYK15cUx%2F%2FY4vysotL28EJ9TfhRNpYgayzUaVeWsU4XrPHuOlQWFgPeDqSJaSTfteaNCndCDVhkjWOp2FpxxPzRd%2BXHh%2FfIBwlheOk0WWbBc1gKz6XKFRuGXAw1FoE2h1BxSauJqU%2FvmFOV7cuATkRvXIR2PVbyeeN68pLgGF0rPk9ws%2BuFgf%2B8ps%2BZfXzHGCR1niDkRRDwS9w9sZl0Zv2aYJevc1ggJ7uiIQf544iCJNlUa2uZ5lulNdtmdPO5EdEDKXHWr2uSMa5HX%2F0%2Bv3xZDdHKTmsj%2Ffu2JgckAxZzbuiJtcZtuvg0eVUjuq27G6jYo2AHZSlaPge1tJwtrizqDI9Rvz98RkavPrO7uUYufwlgjCj42ULKDxQr0yxLDvxNTp9DIGTchWd579YpcAn7NlLBlFjhzDuyq7FBjreAgbuY2l45UrVmIRvlUDki%2BzDfqco%2FBJPgVsh%2FW1lU83gtvwSSGRInbWP%2BWhuYvO%2BDPzeIqL9qGtaxqxS1pagy%2BYcjNhWzfU1WRsXPF562%2FBPUBvEd4iZEpaMowdJnmJeTb%2BtSGxYmODKZx2XEIhK54TbL3n6vg5W8gQoSUUuoSaIy5PR21bT6HWZ0P%2Br3eMTmf2W34goR8q6SvQ6wtg%2F6Q%2Bw%2BhChwdAEICZ%2BmBvj1g%2Bh%2F9YNq2QjFjRILQAL9oTebqtLjB1mzRWFCGReTk%2BgMCqk1GkjrYpZSWN7NSqJ1wpZSwFki50ntt%2FTedm152oMCkCKXElxxFxikKTK1LsOe%2FjLY9UJQhRCPYr2%2Fby4%2BnveK0EaRtPefQKZGAjQRBFb2gr32XfmJfzeGCe40kmybldmw%2B2DPntUFe2BBKFttvIK3cooEvYsvZZUwN9zrLtEgEvfhs4sJFDD9cgJN702&X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Credential=ASIARH4WOD4KP32GAAXX%2F20250825%2Fus-west-1%2Fs3%2Faws4_request&X-Amz-Date=20250825T000110Z&X-Amz-Expires=240&X-Amz-SignedHeaders=host&X-Amz-Signature=5866c4bccbf04564a95eb55781dc113b576262e3e2781b6dd4b2f0873d43542a",
-        port: "",
-        pathname: "/**",
-      },
-    ],
-  },
-};
+const nextConfig = {};
 
 export default nextConfig;
